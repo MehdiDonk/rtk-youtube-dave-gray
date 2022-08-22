2c460cd6ae458df959b53c1e3204954ab41a1c96
Mehdi <mehdi.naqi@gmail.com>
2022-08-22 17:40:17 +0200

    Finishing static post + boilerplate for async

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,54 @@
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+
+const initialState = {
+  posts: [],
+  status: "idle", // 'idle | 'loading' | 'succeeded' | 'failed'
+  error: null,
+};
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        // .push: Only works inside createSlice. It's thanks to Immer.js
+        state.posts.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
+          },
+        };
+      },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.posts.find((post) => post.id === postId);
+      if (existingPost) {
+        existingPost.reactions[reaction]++; // Mutating the state but is ok thanks to ImmerJS inside CreateSlice
+      }
+    },
+  },
+});
+
+// Make it easier when shape of the state changes (only place to change is here)
+export const selectAllPosts = (state) => state.posts.posts;
+
+export const { postAdded, reactionAdded } = postsSlice.actions;
+
+export default postsSlice.reducer;
